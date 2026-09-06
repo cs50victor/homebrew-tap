de6f8bf8df6cc834643a62b68611e15c39295caa
Victor A. <52110451+cs50victor@users.noreply.github.com>
2026-09-06 00:57:55 -0400

    chore(tracer): release v0.1.2 on Homebrew

--- Formula/tracer.rb
+++ Formula/tracer.rb
@@ -8,21 +8,21 @@ class Tracer < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/cs50victor/tracer/releases/download/v0.1.1/tracer_0.1.1_darwin_arm64.tar.gz"
-      sha256 "cedfda421cfbb37aeeab5af115f88d6b1abb819ac2f5d86ee08cf38dc525b19c"
+      url "https://github.com/cs50victor/tracer/releases/download/v0.1.2/tracer_0.1.2_darwin_arm64.tar.gz"
+      sha256 "95c7047f795de0424868703f6f4a4c23a32b368fc2d66b9c96e4c4dc3d5a38d2"
     else
-      url "https://github.com/cs50victor/tracer/releases/download/v0.1.1/tracer_0.1.1_darwin_amd64.tar.gz"
-      sha256 "1899c39d40884b5224f9ce9bbd29f502c2fe199f1a6a40f5fcb518d7333bbc66"
+      url "https://github.com/cs50victor/tracer/releases/download/v0.1.2/tracer_0.1.2_darwin_amd64.tar.gz"
+      sha256 "8e9fcb90f9f567d4c4d8ba530e8de881db5b3a3e7f5ccd09de77931410035762"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/cs50victor/tracer/releases/download/v0.1.1/tracer_0.1.1_linux_arm64.tar.gz"
-      sha256 "94fc89cad6166e0e739ff25b4f7ee75e95b917e35296073b198fbe2632c30830"
+      url "https://github.com/cs50victor/tracer/releases/download/v0.1.2/tracer_0.1.2_linux_arm64.tar.gz"
+      sha256 "714784bc4306f207d76c7a99987e7c5a8c810bf44b6135a7ba398cc1990099ed"
     else
-      url "https://github.com/cs50victor/tracer/releases/download/v0.1.1/tracer_0.1.1_linux_amd64.tar.gz"
-      sha256 "3ce90881d9ed31285c9877b076579821bb542c166519d9a87b42b60a53be3d83"
+      url "https://github.com/cs50victor/tracer/releases/download/v0.1.2/tracer_0.1.2_linux_amd64.tar.gz"
+      sha256 "5223a8200924d46a24f43a3f51c69b5261d5e9f9d51fd2fb4ce220f00fdf3e07"
     end
   end
 
@@ -32,8 +32,9 @@ class Tracer < Formula
 
   def caveats
     <<~EOS
-      Code previews require Ghostty. On macOS, install it with:
-        brew install --cask ghostty
+      Code previews use Zed by default. On macOS, install it with:
+        brew install --cask zed
+      Ghostty is optional for terminal previews.
     EOS
   end
 
